840ca236e0532f9f3d0c5564b84c63b154db3863
Brian <bmaitner@gmail.com>
2024-09-30 16:43:01 -0400

    Added function to pull model statistics

---
+++ R/OpenRange_get_stats.R
@@ -0,0 +1,25 @@
+
+#'Download model performance statistics
+#'
+#'OpenRange_get_stats provides information on model performance.
+#' @param ... Additional arguments passed to internal functions.
+#' @return List object containing one data.frame for each of the three modelling frameworks.
+#' @examples \dontrun{
+#'
+#' range_stats <- OpenRange_get_stats()
+#'
+#'}
+#' @family range functions
+#' @family metadata functions
+#' @export
+OpenRange_get_stats <- function(...){
+  
+  out <- 
+  list(rangebagging = ranges_sql(query = "select * from ranges.statistics_rangebag ;",...),
+       ppm = ranges_sql(query = "select * from ranges.statistics_ppm ;",...),
+       points = ranges_sql(query = "select * from ranges.statistics_points ;",...))
+  
+
+  
+  
+}
