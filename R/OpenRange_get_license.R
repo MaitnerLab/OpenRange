743c352c872513c4a4489d5b3106359caca6707d
Brian <bmaitner@gmail.com>
2024-10-02 14:44:38 -0400

    function to get license information

---
+++ R/OpenRange_get_license.R
@@ -0,0 +1,19 @@
+#'Download license information
+#'
+#'OpenRange_get_license provides information on model licenses.
+#' @param ... Additional arguments passed to internal functions.
+#' @return Data.frame containing information on the license associated with the maps.
+#' @examples \dontrun{
+#'
+#' range_license_info <- OpenRange_get_license()
+#'
+#'}
+#' @family range functions
+#' @family metadata functions
+#' @export
+OpenRange_get_license <- function(...){
+  
+    return(ranges_sql(query = "select * from ranges.license ;", ...))
+    
+  
+}
