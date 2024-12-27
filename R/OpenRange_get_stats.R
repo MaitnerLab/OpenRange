a087f29de0416215032c404693f6e7d6b430262a
Brian <bmaitner@gmail.com>
2024-12-27 11:42:52 -0500

    modelling -> modeling

--- R/OpenRange_get_stats.R
+++ R/OpenRange_get_stats.R
@@ -3,7 +3,7 @@
 #'
 #'OpenRange_get_stats provides information on model performance.
 #' @param ... Additional arguments passed to internal functions.
-#' @return List object containing one data.frame for each of the three modelling frameworks.
+#' @return List object containing one data.frame for each of the three modeling frameworks.
 #' @examples \donttest{
 #'
 #' range_stats <- OpenRange_get_stats()
